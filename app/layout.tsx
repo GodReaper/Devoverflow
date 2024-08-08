5a46ef3a08ee2daaeedd1a36f5c9a7b82644a0cc
Raj Utso <brajutso@gmail.com>
2024-08-08 09:45:29 +0530

    fonts and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,27 @@
 import { ClerkProvider, UserButton } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description: "The community for developers",
+  icons: {
+    icon: "/assests/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,9 +29,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover: text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           {children}
           <UserButton />
         </body>
